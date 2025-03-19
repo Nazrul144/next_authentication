310b5ac0c17170e7dfcc230d12d05ef343c8f88f
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-19 15:18:33 +0600

    Active routing implemented with purple color and font bold

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,7 +1,10 @@
+'use client'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
+    const pathName = usePathname();
     const navlinks = [
         {
             title: 'Home',
@@ -20,9 +23,9 @@ const Navbar = () => {
         <div>
             <div className='flex bg-orange-500 justify-between text-xl text-white lg:px-20 px-2'>
                     
-                    <div className='flex justify-end gap-3 bg-red-500 mr-auto'>
+                    <div className='flex justify-end gap-3  '>
                     {
-                        navlinks?.map((navlink)=> <Link key={navlink.path} href={navlink.path}>{navlink.title}</Link>)
+                        navlinks?.map((navlink)=> <Link className={`${pathName === navlink.path ? "font-bold text-purple-700" : ""}`} key={navlink.path} href={navlink.path}>{navlink.title}</Link>)
                     }
                     </div>
                     <div>Login</div>
