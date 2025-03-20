6fe1fefb2c58a438b163c07f6276a8f16d9ee62b
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-20 21:59:16 +0600

    Authentication Setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import AuthProvider from "@/services/AuthProvider";
 
 
 const geistSans = Geist({
@@ -24,11 +25,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthProvider>
         <Navbar/>
         {children}
+        </AuthProvider>        
       </body>
     </html>
   );
